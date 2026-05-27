610ccb9501ef8e4df515969e33e267e94706fde9
Mai Quoc Viet <duvimuahe123@gmail.com>
2026-05-27 22:07:45 +0700

    update

--- app/version.py
+++ app/version.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
 
-APP_VERSION = "0.1.1"
+APP_VERSION = "0.1.2"
 UPDATE_MANIFEST_URL = "https://raw.githubusercontent.com/maiviet283/meida_harvester/main/update.json"
