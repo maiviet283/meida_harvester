3c5c0d33a69fc3a0a219eb1350ca1a989ee37600
Mai Quoc Viet <duvimuahe123@gmail.com>
2026-06-03 12:01:50 +0700

    Release v0.1.16

--- app/version.py
+++ app/version.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
 
-APP_VERSION = "0.1.15"
+APP_VERSION = "0.1.16"
 UPDATE_MANIFEST_URL = "https://course-finder-3v7y.onrender.com/licenses/version/"
 BUY_KEY_URL = "https://t.me/vietdonate"
